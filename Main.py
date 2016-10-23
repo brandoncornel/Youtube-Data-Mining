df1a88d6fe7bca9c98d1d2958f06c4476c3ba5d0
Brandon Cornel <bcc4679@rit.edu>
2016-10-23 16:30:20 -0400

    Added graph for dataset information

--- Main.py
+++ Main.py
@@ -1,6 +1,40 @@
+import csv
+import os
+import math
+import matplotlib.pyplot as plt
+from collections import Counter
+import numpy
+
+dir = os.path.dirname(__file__)
+
 class Main:
     def main(self):
-        print ("YOOO")
+        self.dataSetInformationParse()
+
+    def dataSetInformationParse(self):
+        rows = []
+        filename = os.path.join(dir, 'Dataset_Information.csv')
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if (reader.line_num != 1):
+                    rows.append(row)
+        totalNumVideos = 0
+        valsX = []
+        valsY = []
+        for row in rows:
+            totalNumVideos+=int(row[3])
+            valsX.append(row[0])
+            valsY.append(row[3])
+        print "Total Number of Videos Crawled: ", totalNumVideos
+        plt.scatter(valsX, valsY, label='Crawl', color='k')
+        plt.xlabel('Crawl ID')
+        plt.ylabel('Number of Videos')
+        plt.title('Number of Videos Crawled')
+        plt.legend()
+        plt.show()
+
+
 
 
 Main().main()
