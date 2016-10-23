09734a3e4ce37a81c8d092d86f9cabf9e4937a54
Brandon Cornel <bcc4679@rit.edu>
2016-10-23 17:46:43 -0400

    *Added parser for each file in a folder

--- Main.py
+++ Main.py
@@ -4,14 +4,22 @@ import math
 import matplotlib.pyplot as plt
 from collections import Counter
 import numpy
+import glob
 
 dir = os.path.dirname(__file__)
 
 class Main:
     def main(self):
         self.dataSetInformationParse()
+        path = "datasets/080725"
+        filename = os.path.join(path, '0.txt')
+        with open(filename, 'r') as f:
+            for line in f:
+                print line
+
 
     def dataSetInformationParse(self):
+        print "Parsing and Graphing Dataset Information..."
         rows = []
         filename = os.path.join(dir, 'Dataset_Information.csv')
         with open(filename, 'r') as f:
@@ -28,11 +36,24 @@ class Main:
             valsY.append(row[3])
         print "Total Number of Videos Crawled: ", totalNumVideos
         plt.scatter(valsX, valsY, label='Crawl', color='k')
+        plt.plot(valsX, valsY)
         plt.xlabel('Crawl ID')
         plt.ylabel('Number of Videos')
         plt.title('Number of Videos Crawled')
         plt.legend()
         plt.show()
+
+        #for row in rows:
+            #self.parseEachFolder(int(row[0]), row[1])
+
+    def parseEachFolder(self, ID, folderName):
+        print "Parsing and Graphing for crawl ID", ID
+        rows = []
+        path = "datasets/"+ folderName
+        #for filename in glob.iglob(os.path.join(path, '*.txt')):
+            #with open(filename) as f:
+                #for line in f:
+        filename = os.path.join(path, 'Dataset_Information.csv')
 
 
 
